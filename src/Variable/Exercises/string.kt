aeab20346b12e6e8780fbc430e455c592cecd7e1
Coding Algorithm Alchemist <codingalgorithmjourney@gmail.com>
2024-11-07 08:33:46 +0300

    learning about string and it's function

---
+++ src/Variable/Exercises/string.kt
@@ -0,0 +1,43 @@
+package Variable.Exercises
+
+fun main(){
+    /*
+    Kotlin Strings
+    Strings are used for storing text.
+
+    A string contains a collection of characters surrounded by double quotes
+
+   Access a String
+   To access the characters (elements) of a string, you must refer to the index number inside square brackets
+
+
+String Length
+A String in Kotlin is an object, which contain properties and functions that can perform certain operations on strings, by writing a dot character (.) after the specific string variable. For example, the length of a string can be found with the length property:
+
+   String Functions
+There are many string functions available
+
+Comparing Strings
+The compareTo(string) function compares two strings and returns 0 if both are equal
+
+Finding a String in a String
+The indexOf() function returns the index (the position) of the first occurrence of a specified text in a string (including whitespace)
+
+String Concatenation
+The + operator can be used between strings to add them together to make a new string. This is called concatenation
+You can also use the plus() function to concatenate two strings
+
+
+
+String Templates/Interpolation
+Instead of concatenation, you can also use "string templates", which is an easy way to add variables and expressions inside a string.
+
+Just refer to the variable with the $ symbol
+var firstName = "John"
+var lastName = "Doe"
+println("My name is $firstName $lastName")
+
+
+
+     */
+}
