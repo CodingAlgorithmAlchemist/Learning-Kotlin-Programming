66ec2b4bdf88b219c277e28b9057087d1b441b04
Coding Algorithm Alchemist <codingalgorithmjourney@gmail.com>
2024-11-07 08:13:13 +0300

    learning about operators

---
+++ src/Variable/operator.kt
@@ -0,0 +1,41 @@
+package Variable
+
+fun main(){
+    /*
+    Kotlin Operators
+    Operators are used to perform operations on variables and values.
+    The value is called an operand, while the operation (to be performed between the two operands) is defined by an operator
+
+
+    Kotlin divides the operators into the following groups:
+    Arithmetic operators are used to perform common mathematical operations
+        +	Addition	Adds together two values
+        -	Subtraction	Subtracts one value from another
+        *	Multiplication	Multiplies two values
+        /	Division	Divides one value from another
+        %	Modulus	Returns the division remainder
+        +   Increment	Increases the value by 1
+        --	Decrement	Decreases the value by 1
+
+
+    Assignment operators are used to assign values to variables
+     var x = 10
+
+
+    Comparison operators  are used to compare two values, and returns a Boolean value: either true or false.
+        ==	Equal to
+        !=	Not equal
+        >	Greater than
+        <	Less than
+        >=	Greater than or equal to
+        <=	Less than or equal to
+
+
+    Logical operators  are used to determine the logic between variables or values:
+        && 	Logical and	Returns true if both statements are true
+        || 	Logical or	Returns true if one of the statements is true
+        !	Logical not	Reverse the result, returns false if the result is true
+
+
+    */
+}
